50e6e11a2549d1c10b8be569ff360ebc42aaa54e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-03-17 07:29:46 +0000

    Use rails/all and add sqlite3

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,6 @@
 require_relative 'boot'
 
-# require 'rails/all'
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
-require "action_cable/engine"
-
+require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
